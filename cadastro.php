4125a9f4a4443018ef36806a2d97ad316b33ccb8
Fernando Rocha <beneditofernandor@gmail.com>
2022-02-07 10:28:37 -0300

    init

---
+++ cadastro.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+require_once 'config.php';
+
+if(!empty($_POST['nome']) && !empty($_POST['email'])){
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $id_pai = $_SESSION['mmnlogin'];
+    $senha = md5($email);
+
+
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $sql->bindValue(":email", $email);
+    $sql->execute();
+
+    if($sql->rowCount() == 0){
+        $sql = $pdo->prepare("INSERT INTO usuarios (id_pai, nome, email, senha) VALUES (:id_pai, :nome, :email, :senha)");
+
+        $sql->bindValue(":id_pai", $id_pai);
+        $sql->bindValue(":nome", $nome);
+        $sql->bindValue(":email",$email);
+        $sql->bindValue(":senha", $senha);
+        $sql->execute();
+
+        header("Location: index.php");
+        exit;
+    }else{
+        echo "Já existe um usuário cadastrado com esse E-mail";
+    }
+    
+}
+
+
+?>
+
+<h1>Cadastrar Novo Usuário</h1>
+<form method="POST">
+    Nome: <br>
+    <input type="text" name="nome"><br><br>
+
+    E-mail: <br>
+    <input type="email" name="email"> <br><br>
+
+    <input type="submit" value="Cadastrar">
+
+
+
+</form>
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+    try{
+        global $pdo;
+        $pdo = new PDO("mysql:dbname=projeto_mmn;host=localhost","root","");
+
+    }catch(PDOException $e){
+        echo "ERRO: ".$e;
+    }
+$limite = 3;
+
+$patentes = array(
+        
+
+);
+
+?>
